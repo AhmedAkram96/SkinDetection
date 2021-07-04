89f81a5cfb742551dec8a460ca7e50cb301b422b
root <ahmed.akram32765@gmail.com>
2021-07-04 12:22:04 +0000

    changed the mask result to be black and white reversed and added a new test image

--- SkinDetection.py
+++ SkinDetection.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 #Open a simple image
-img=cv2.imread("6_A_hgr2B_id05_1.jpg")
+img=cv2.imread("akram.png")
 
 #converting from gbr to hsv color space
 img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -22,18 +22,11 @@ global_mask=cv2.medianBlur(global_mask,3)
 global_mask = cv2.morphologyEx(global_mask, cv2.MORPH_OPEN, np.ones((4,4), np.uint8))
 
 
-HSV_result = cv2.bitwise_not(HSV_mask)
-YCrCb_result = cv2.bitwise_not(YCrCb_mask)
-global_result=cv2.bitwise_not(global_mask)
+# HSV_result = cv2.bitwise_not(HSV_mask)
+# YCrCb_result = cv2.bitwise_not(YCrCb_mask)
+# cv2.imwrite("1_HSV.jpg",HSV_result)
+# cv2.imwrite("2_YCbCr.jpg",YCrCb_result)
 
 
-#show results
-# cv2.imshow("1_HSV.jpg",HSV_result)
-# cv2.imshow("2_YCbCr.jpg",YCrCb_result)
-# cv2.imshow("3_global_result.jpg",global_result)
-# cv2.imshow("Image.jpg",img)
-cv2.imwrite("1_HSV.jpg",HSV_result)
-cv2.imwrite("2_YCbCr.jpg",YCrCb_result)
-cv2.imwrite("3_global_result.jpg",global_result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()  
+cv2.imwrite("3_global_result.jpg",global_mask)
+  
